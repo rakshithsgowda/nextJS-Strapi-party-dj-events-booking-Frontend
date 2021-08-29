d7ca1e9f51b11b3f56636ff18dedde65b98567d8
rakshith <rakshit.s.gowda@gmail.com>
2021-08-29 18:54:26 +0530

    Head tags and Layout

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,11 @@
+import Layout from '../components/Layout'
+
 export default function AboutPage() {
   return (
-    <div>
+    <Layout title='about DJ EVENTS'>
       <h1>About</h1>
       <p>This is an app to find the latest DJ and other musical events</p>
-      <p>
-        Version:1.0.0
-      </p>
-    </div>
+      <p>Version:1.0.0</p>
+    </Layout>
   )
 }
--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,7 +1,9 @@
+import Layout from '../../components/Layout'
+
 export default function EventPage() {
   return (
-    <div>
+    <Layout>
       <h1>My slug Event</h1>
-    </div>
+    </Layout>
   )
 }
