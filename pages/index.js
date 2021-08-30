9162a13286492f957656d1fa67732e351de310cd
rakshith <rakshit.s.gowda@gmail.com>
2021-08-30 12:20:03 +0530

    displaying Events

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,23 @@
+import EventItem from '@/components/EventItem'
 import Layout from '@/components/Layout'
 import { API_URL } from '@/config/index.js'
+import Link from 'next/link'
 
 export default function HomePage({ events }) {
   return (
     <Layout>
       <h1>Upcoming Events</h1>
+      {events.length === 0 && <h3>No Events to show</h3>}
+
+      {events.map((evt) => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
+
+      {events.length > 0 && (
+        <Link href='/events'>
+          <a className='btn-secondary'>Events</a>
+        </Link>
+      )}
     </Layout>
   )
 }
