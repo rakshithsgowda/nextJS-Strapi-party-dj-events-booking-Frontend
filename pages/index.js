2e6a5147a55154e29e5d82c861dd74bfaab24e4d
rakshith <rakshit.s.gowda@gmail.com>
2021-08-30 11:05:26 +0530

    apis serverside and staticprops

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,20 @@
 import Layout from '@/components/Layout'
+import { API_URL } from '@/config/index.js'
 
-const HomePage = () => {
+export default function HomePage({ events }) {
   return (
     <Layout>
-      <h1>Home</h1>
+      <h1>Upcoming Events</h1>
     </Layout>
   )
 }
 
-export default HomePage
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`)
+  const events = await res.json()
+
+  return {
+    props: { events },
+    revalidate: 1,
+  }
+}
